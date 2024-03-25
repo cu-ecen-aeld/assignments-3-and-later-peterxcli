00eaea273fdfc1587dab457420a11a565fe7dde7
peterxcli <peterxcli@gmail.com>
2024-03-25 07:32:35 +0000

    fix: Remove aesdchar driver load and unload commands from aesdsocket-start-stop.sh

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -2,15 +2,11 @@
 
 case "$1" in
 	start)
-	  echo "Load aesdchar driver"
-	  aesdchar_load
 	  
 	  echo "Starting aesdsocket server"
 	  start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
 	  ;;
 	stop)
-	  echo "Unload aesdchar driver"
-	  aesdchar_unload
 	  
 	  echo "Stopping aesdsocket server"
 	  start-stop-daemon -K -n aesdsocket --signal SIGTERM
