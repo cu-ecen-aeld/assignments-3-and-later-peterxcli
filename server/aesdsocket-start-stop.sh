fc4e8ff639d08b3133a62d877fb03dc76aaa7e39
peterxcli <peterxcli@gmail.com>
2024-03-25 07:29:06 +0000

    fix: Refactor aesdsocket-start-stop.sh script

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -1,30 +1,23 @@
-#!/bin/bash
-
-DAEMON_PATH="/usr/bin"
-DAEMON_NAME="aesdsocket"
-DAEMON="$DAEMON_PATH/$DAEMON_NAME"
-DAEMON_OPTS="-d"
-PIDFILE="$DAEMON_PATH/$DAEMON_NAME.pid"
+#! /bin/sh
 
 case "$1" in
-    start)
-        echo "Starting $DAEMON_NAME..."
-        start-stop-daemon --start --quiet --background --make-pidfile --pidfile $PIDFILE --exec $DAEMON -- $DAEMON_OPTS
-        ;;
-    stop)
-        echo "Stopping $DAEMON_NAME..."
-        start-stop-daemon --stop --quiet --pidfile $PIDFILE
-        if [ -e $PIDFILE ]; then
-            rm $PIDFILE
-        fi
-        ;;
-    restart)
-        $0 stop
-        $0 start
-        ;;
-    *)
-        echo "Usage: $0 {start|stop|restart}"
-        exit 1
+	start)
+	  echo "Load aesdchar driver"
+	  aesdchar_load
+	  
+	  echo "Starting aesdsocket server"
+	  start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
+	  ;;
+	stop)
+	  echo "Unload aesdchar driver"
+	  aesdchar_unload
+	  
+	  echo "Stopping aesdsocket server"
+	  start-stop-daemon -K -n aesdsocket --signal SIGTERM
+	  ;;
+	*)
+	  echo "Usage: $0 {start | stop}"
+	exit 1
 esac
 
 exit 0
